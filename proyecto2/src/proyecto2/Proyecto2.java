d37bd371b7a5ea4995b7d4bb01ce64868db7f3c2
Jeremy Live <jeremylive@live.com>
2017-10-11 18:46:02 -0600

    Esqueleto de la progra fase1

---
+++ proyecto2/src/proyecto2/Proyecto2.java
@@ -0,0 +1,16 @@
+package proyecto2;
+
+/**
+ *
+ * @author live
+ */
+public class Proyecto2 
+{
+    public static void main(String[] args) 
+    {
+        new ventanaPrincipal().setVisible(true);
+                
+        
+    }
+    
+}
